74eed3e41fa6bd755e52c5264592151aba39081a
alireza parvaresh <89921883+parvvaresh@users.noreply.github.com>
2022-10-14 23:43:38 +0330

    face dection

---
+++ face dection/main.py
@@ -0,0 +1,30 @@
+import cv2
+
+faceCascade = cv2.CascadeClassifier("/home/alireza/Desktop/test/haarcascade_frontalface_default.xml")
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    
+video_capture.release()
+cv2.destroyAllWindows()
